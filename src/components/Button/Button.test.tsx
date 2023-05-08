e0164de1e24781a0af0d8d587c1a986d99886196
vinicius costa <vinicius.dsc95@gmail.com>
2023-05-08 08:43:10 -0300

    ci: jest and react-testing-library setup

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,12 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Button } from '.';
 
 const testId = 'Button-test-id';
 
 describe('<Button />', () => {
   test('Test if Button component renders correctly', () => {
-    render(<Button data-testid={testId} />);
-    const element = screen.getByTestId(testId);
+    const { getByTestId } = render(<Button data-testid={testId} />);
+    const element = getByTestId(testId);
     expect(element).toBeInTheDocument();
   });
 });
