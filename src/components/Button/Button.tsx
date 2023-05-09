2782f20bb397a5cee69463673ebae9738d3b2ff4
vinicius costa <vinicius.dsc95@gmail.com>
2023-05-09 12:39:53 -0300

    style: basic button style

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,18 @@
+import { cn } from '../../lib';
+
 export interface ButtonProps
   extends React.HtmlHTMLAttributes<HTMLButtonElement> {}
 
-export function Button({ children, ...props }: ButtonProps) {
-  return <button {...props}>{children}</button>;
+export function Button({ className, children, ...props }: ButtonProps) {
+  return (
+    <button
+      className={cn(
+        'inline-flex px-6 py-3 rounded-lg bg-zinc-800 text-zinc-200 dark:bg-zinc-200 dark:text-zinc-800 font-sans text-base font-medium',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
